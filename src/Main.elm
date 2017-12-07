af4f428dbe5df405671711c70c9a3ad0111fd2cb
Ole Anders Eidjord <oae@vaf.no>
2017-12-07 14:53:24 +0100

    Lagt til tastatursnarveier, oppdateres hver 0.1 sekund

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,13 @@
 module Main exposing (..)
 
-import Html exposing (Attribute, Html, button, div, h1, img, p, text)
+import Char
+import Html exposing (Attribute, Html, button, div, h1, img, p, span, text)
 import Html.Attributes exposing (class, style)
 import Html.Events exposing (keyCode, on, onClick)
-import Task
-import Time exposing (Time, every)
-import Time.DateTime as DateTime exposing (DateTime, DateTimeDelta, hour, minute, second)
-import Keyboard
+import Keyboard exposing (KeyCode)
+import List
+import Time exposing (Time, every, second)
+import Time.DateTime as DateTime exposing (DateTime, DateTimeDelta, hour, minute)
 
 
 ---- MODEL ----
@@ -18,19 +19,31 @@ type alias Model =
     , sluttTid : DateTime
     , nedtelling : DateTimeDelta
     , prosentFerdig : Float
+    , valg : List Valg
+    }
+
+
+type alias Valg =
+    { minutter : Int
+    , hurtigtast : KeyCode
     }
 
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
-    ( { gjeldendeTid = DateTime.epoch
-      , startTid = DateTime.epoch
-      , sluttTid = DateTime.epoch
-      , nedtelling = deltaZero
-      , prosentFerdig = 0
-      }
-    , getTime
-    )
+    { gjeldendeTid = DateTime.epoch
+    , startTid = DateTime.epoch
+    , sluttTid = DateTime.epoch
+    , nedtelling = deltaZero
+    , prosentFerdig = 0
+    , valg =
+        [ Valg 1 (Char.toCode 'R')
+        , Valg 3 (Char.toCode 'I')
+        , Valg 5 (Char.toCode 'H')
+        , Valg 10 (Char.toCode 'S')
+        ]
+    }
+        ! []
 
 
 deltaZero : DateTimeDelta
@@ -38,17 +51,23 @@ deltaZero =
     DateTime.delta DateTime.epoch DateTime.epoch
 
 
-prosentFerdig : Model -> Float
-prosentFerdig model =
+beregnProsentFerdig : DateTime -> DateTime -> DateTime -> Float
+beregnProsentFerdig gjeldendeTid startTid sluttTid =
     let
         gjeldende =
-            DateTime.delta model.gjeldendeTid model.startTid
+            DateTime.delta gjeldendeTid startTid
 
         totalt =
-            DateTime.delta model.sluttTid model.startTid
+            DateTime.delta sluttTid startTid
+
+        prosent_ =
+            (toFloat gjeldende.milliseconds) / (toFloat totalt.milliseconds) * 100
 
         prosent =
-            (toFloat gjeldende.milliseconds) / (toFloat totalt.milliseconds) * 100
+            if prosent_ < 100 then
+                prosent_
+            else
+                100
     in
         prosent
 
@@ -60,7 +79,7 @@ prosentFerdig model =
 type Msg
     = OnTime Time
     | SettMaltid Int
-    | Keypress Keyboard.KeyCode
+    | Keypress KeyCode
 
 
 type Key
@@ -76,34 +95,34 @@ update msg model =
                 b =
                     x model t
             in
-                ( b
-                , Cmd.none
-                )
+                b ! []
 
         Keypress key ->
             let
-                nyTid minutter =
-                    DateTime.addMinutes minutter model.gjeldendeTid
-
                 _ =
                     Debug.log "Keypress!" key
 
                 tid =
-                    case key of
-                        13 ->
-                            nyTid 20
-
-                        _ ->
-                            nyTid 30
+                    model.valg
+                        |> List.filter (\x -> x.hurtigtast == key)
+                        |> List.map (\x -> x.minutter)
+                        |> List.head
+                        |> Maybe.withDefault 0
+                        |> settNySluttTid model.gjeldendeTid
             in
-                ( { model | startTid = model.gjeldendeTid, sluttTid = tid }, getTime )
+                { model | startTid = model.gjeldendeTid, sluttTid = tid } ! []
 
         SettMaltid minutter ->
             let
-                nyTid =
-                    DateTime.addMinutes minutter model.gjeldendeTid
+                nySluttTid =
+                    settNySluttTid model.gjeldendeTid minutter
             in
-                ( { model | startTid = model.gjeldendeTid, sluttTid = nyTid }, getTime )
+                { model | startTid = model.gjeldendeTid, sluttTid = nySluttTid } ! []
+
+
+settNySluttTid : DateTime -> Int -> DateTime
+settNySluttTid gjeldendeTid minutter =
+    DateTime.addMinutes minutter gjeldendeTid
 
 
 x : Model -> Time -> Model
@@ -121,14 +140,8 @@ x model t =
             else
                 opptalt
 
-        prosent_ =
-            prosentFerdig model
-
         prosent =
-            if prosent_ < 100 then
-                prosent_
-            else
-                100
+            beregnProsentFerdig model.gjeldendeTid model.startTid model.sluttTid
     in
         { model
             | gjeldendeTid = gjeldende
@@ -143,25 +156,25 @@ x model t =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ h1 [] [ text "Taletid" ]
-        , p [] [ text <| formaterTidspunkt2 <| model.nedtelling ]
-        , viewProgressbar model.prosentFerdig
-        , visKnapp 1
-        , visKnapp 3
-        , visKnapp 5
-        , visKnapp 10
-        , button
-            [ onClick <| SettMaltid 0 ]
-            [ text "Nullstill" ]
-        ]
+    div [] <|
+        List.concat
+            [ [ h1 [] [ text "Taletid" ]
+              , p [] [ text <| formaterTidspunkt2 <| model.nedtelling ]
+              , viewProgressbar model.prosentFerdig
+              ]
+            , List.map viewKnapp model.valg
+            , [ button
+                    [ onClick <| SettMaltid 0 ]
+                    [ text "Nullstill" ]
+              ]
+            ]
 
 
-visKnapp : Int -> Html Msg
-visKnapp antallMin =
+viewKnapp : Valg -> Html Msg
+viewKnapp valg =
     button
-        [ onClick <| SettMaltid antallMin ]
-        [ text <| toString antallMin ++ " min" ]
+        [ onClick <| SettMaltid valg.minutter ]
+        [ text <| toString valg.minutter ++ " min" ]
 
 
 viewProgressbar : Float -> Html Msg
@@ -175,34 +188,22 @@ viewProgressbar prosent =
         ]
 
 
-formaterTidspunkt : DateTime -> String
-formaterTidspunkt gjeldendeTidspunkt =
-    let
-        lagTid tidstype =
-            gjeldendeTidspunkt
-                |> tidstype
-                |> toString
-                |> String.padLeft 2 '0'
-    in
-        lagTid hour
-            ++ ":"
-            ++ lagTid minute
-            ++ ":"
-            ++ lagTid second
-
-
 formaterTidspunkt2 : DateTimeDelta -> String
 formaterTidspunkt2 gjeldendeTidspunkt =
-    String.padLeft 2 '0' (toString (gjeldendeTidspunkt.minutes))
-        ++ ":"
-        ++ String.padLeft 2 '0' (toString (gjeldendeTidspunkt.seconds - gjeldendeTidspunkt.minutes * 60))
+    let
+        padd str =
+            String.padLeft 2 '0' (toString (str))
+    in
+        padd gjeldendeTidspunkt.minutes
+            ++ ":"
+            ++ padd (gjeldendeTidspunkt.seconds - gjeldendeTidspunkt.minutes * 60)
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch
         [ Keyboard.downs Keypress
-        , every Time.second OnTime
+        , every (0.1 * second) OnTime
         ]
 
 
@@ -212,12 +213,6 @@ subscriptions model =
 
 type alias Flags =
     {}
-
-
-getTime : Cmd Msg
-getTime =
-    Time.now
-        |> Task.perform OnTime
 
 
 main : Program Flags Model Msg
